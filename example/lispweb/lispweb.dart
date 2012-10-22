20ce294919817aa032b39d2a6fce7164bf1ec01e
Lukas Renggli <renggli@gmail.com>
2012-10-23 00:49:04 +0200

    - moved resources to example dictionary - got rid of default.lisp and use an internal string - fixed the lisp define function

---
+++ example/lispweb/lispweb.dart
@@ -0,0 +1,37 @@
+// Copyright (c) 2012, Lukas Renggli <renggli@gmail.com>
+
+library lispweb;
+
+import 'dart:html';
+import 'package:petitparser/petitparser.dart';
+import 'package:petitparser/lisp.dart';
+
+void inspector(Element element, Environment environment) {
+  var result = '';
+  while (environment != null) {
+    result = '$result<ul>';
+    for (var symbol in environment.keys) {
+      result = '$result<li><b>$symbol</b>: ${environment[symbol]}</li>';
+    }
+    result = '$result</ul>';
+    environment = environment.parent;
+  }
+  element.innerHTML = result;
+}
+
+main() {
+  Parser parser = new LispParser();
+  Environment root = new RootEnvironment();
+  evalString(parser, root, Natives.standardLibrary);
+  Environment environment = root.create();
+
+  TextAreaElement input = query('#input');
+  TextAreaElement output = query('#output');
+
+  query('#evaluate').on.click.add((event) {
+    var result = evalString(parser, environment, input.value);
+    output.innerHTML = result.toString();
+    inspector(query('#inspector'), environment);
+  });
+  inspector(query('#inspector'), environment);
+}
