53b83425bd8d5e7019d2fa63131887cd34edf2ec
Lukas Renggli <renggli@gmail.com>
2017-07-23 00:42:25 +0200

    Move the smalltalk grammar to the examples.

---
+++ example/smalltalk/smalltalk.dart
@@ -0,0 +1,7 @@
+/// This package contains the complete grammar of Smalltalk.
+///
+/// It was automatically exported from PetitParser for Smalltalk.
+library petitparser.example.smalltalk;
+
+export 'src/grammar.dart';
+export 'src/parser.dart';
