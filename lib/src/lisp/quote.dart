a54b027a47870fd7a26e83d9824dc31abea7ee1d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2023-11-07 22:51:37 +0900

    Implement ==, hashCode, toString for Quote

--- lib/src/lisp/quote.dart
+++ lib/src/lisp/quote.dart
@@ -5,4 +5,13 @@ class Quote {
 
   /// The quoted datum.
   dynamic datum;
+
+  @override
+  bool operator ==(Object other) => other is Quote && other.datum == datum;
+
+  @override
+  int get hashCode => 17 * datum.hashCode;
+
+  @override
+  String toString() => "'$datum";
 }
